3e699c5410d878816fcfda9f1ff756091a91151b
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-08-03 11:40:19 +0530

    Update Handtrack.py

--- Handtrack.py
+++ Handtrack.py
@@ -1,7 +1,5 @@
 
-#Required things
-#1.Mediapipe
-#2.OpenCV
+
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
@@ -12,11 +10,10 @@ cap = cv2.VideoCapture(0)
 hands=mp_hands.Hands()
 while True:
     success, image = cap.read()
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
+    
+    
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    
     results = hands.process(image)
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
