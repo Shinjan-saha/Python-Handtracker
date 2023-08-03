27af324a0f0704b53e100caa68851427edcf846c
Shinjan saha <116014977+Shinjan-saha@users.noreply.github.com>
2023-08-03 11:46:55 +0530

    Update BodyTracking_mediapipe.py

--- BodyTracking_mediapipe.py
+++ BodyTracking_mediapipe.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_objectron = mp.solutions.objectron
-# For webcam input:
+
 cap = cv2.VideoCapture(0)
 with mp_objectron.Objectron(static_image_mode=False,
                             max_num_objects=5,
@@ -18,7 +18,7 @@ with mp_objectron.Objectron(static_image_mode=False,
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = objectron.process(image)
 
-    # Draw the box landmarks on the image.
+    
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.detected_objects:
@@ -26,5 +26,5 @@ with mp_objectron.Objectron(static_image_mode=False,
                     mp_drawing.draw_landmarks(image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
                     mp_drawing.draw_axis(image, detected_object.rotation,
                                  detected_object.translation)
-    # Flip the image horizontally for a selfie-view display.
+    
             cv2.imshow('Body tracker', cv2.flip(image, 1))
